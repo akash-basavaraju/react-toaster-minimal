8053b0e63d4776682f280a521bec0c6613b306ac
Akash Basavaraju <akash1.basavaraju@paytm.com>
2020-06-17 23:38:53 +0530

    added rollup for bundling operation

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from "rollup-plugin-typescript2";
+import styles from "rollup-plugin-styles";
+import { terser } from "rollup-plugin-terser";
+import copy from "rollup-plugin-copy";
+import pkg from "./package.json";
+
+export default {
+  input: "./src/index.tsx",
+  output: {
+    file: "./lib/index.js",
+    format: "es",
+    sourcemap: true,
+  },
+  external: [...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    typescript({ declaration: true }),
+    styles({ modules: false }),
+    copy({ targets: [{ src: "src/**/*.css", dest: "lib/" }] }),
+    terser(),
+  ],
+};
